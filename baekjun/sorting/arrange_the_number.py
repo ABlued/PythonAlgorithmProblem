79a6a371d08d78a4d429464a86d0ab22fef23a31
ABlued <53801395+ABlued@users.noreply.github.com>
2021-10-11 15:42:28 +0900

    백준 수 정렬하기 문제 해결

---
+++ baekjun/sorting/arrange_the_number.py
@@ -0,0 +1,4 @@
+N = int(input())
+sorted_list = sorted( [int(input()) for _ in range(N)] )
+
+[print(value) for value in sorted_list]
--- practice/input.py
+++ practice/input.py
@@ -20,4 +20,3 @@
 # arr = [list(map(int, input().split())) for _ in range(5)]
 
 # print(list(map(lambda x: x ** 2 , range(5))))
-
